ac744e3ecc341ef10567473867506142ae16d49b
crigutierrez-code <crigutierrez@uniboyaca.edu.co>
2025-10-15 18:07:41 -0500

    Se crearon las carpetas y la conexion a la base de datos

---
+++ Modelo/conexion.php
@@ -0,0 +1,33 @@
+<?php
+namespace Modelo;
+
+use mysqli;
+use Exception;
+
+class Conexion
+{
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $db = "notas_app";
+    private $conexion;
+
+    public function __construct()
+    {
+        $this->conexion = new mysqli(
+            $this->host,
+            $this->user,
+            $this->pass,
+            $this->db
+        );
+        if ($this->conexion->connect_error) {
+            throw new Exception("Error de conexión: " . $this->conexion->connect_error);
+        }
+    }
+
+    public function getConexion()
+    {
+        return $this->conexion;
+    }
+}
+?>
